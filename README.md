65080f986d7d2705e609cca26177798179aa40df
Lucas Torti <luctort@outlook.com>
2019-12-01 16:47:43 -0800

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Super Car Sorter
+
 This repo contains a bash script named “cars.sh” that will help a user maintain an inventory of old cars.
  
- The user will be presented with a menu of three options:
+The user will be able to type in numbers for commands
+
+1. To input a car into the system
+2. Will display the current list of cars, in order by date
+3. Will quit the program
+4. Any other input, such as "4", will give the user a list of commands
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=_31IFSvC2II
+" target="_blank"><img src="http://img.youtube.com/vi/_31IFSvC2II/0.jpg" 
+alt="CARS" width="240" height="180" border="10" /></a>  
---
+++ cars.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+#cars.sh
+#Lucas Torti
+
+echo ""
+echo "Hello, and welcome to The Super Car Sorter (c) 2019 LucTort Inc."
+
+echo "Please enter a command, or type help for a list of commands:"
+
+inputCommand=99
+
+while ((inputCommand != 3))
+do
+
+	if [ $((inputCommand)) -eq 99 ]
+	then
+	echo "Enter command: "
+fi
+	read inputCommand
+
+case $inputCommand in
+	1)
+		echo "Please enter year:"
+		read year
+		echo "Please enter make:"
+		read make
+		echo "Please enter model: "
+		read model
+
+		carData="$year:$make:$model"
+		echo "Your car data is $carData"
+
+		echo $carData >> My_old_cars
+
+		echo ""
+		;;
+	2)
+		sort My_old_cars > My_old_cars_output
+		file=$(<My_old_cars_output)
+		echo "$file"
+		;;
+		3) 
+			echo "Thank you for using The Super Car Sorter (c) 2019 LucTort Inc. We hope you have a wonderful day."
+			echo ""
+			;;
+
+		*)
+		echo "List of commands:"
+		echo "1 - Allows user to add car to list"
+		echo "2 - Lists the entered cars"
+		echo "3 - Closes the program"
+		echo ""
+		echo "Enter command: "
+		;;
+esac
+done
+
+
+
